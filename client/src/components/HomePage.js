e2e1bb8c724feed946a77be05bba57f22c42d2f8
Clay Mangum <clay@trinitasaccounting.com>
2024-05-23 18:34:37 -0400

    Home page build #3

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -31,7 +31,7 @@ export default function HomePage() {
                         alt="Your Company"
                     />
                     <div className="mt-24 sm:mt-32 lg:mt-16">
-                        <a className="inline-flex space-x-6">
+                        <a href='/' className="inline-flex space-x-6">
                             <span className="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">
                                 What's new
                             </span>
@@ -49,12 +49,12 @@ export default function HomePage() {
                     </p>
                     <div className="mt-10 flex items-center gap-x-6">
                         <a
-                            // href="#"
+                            href="/"
                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Get started
                         </a>
-                        <a className="text-sm font-semibold leading-6 text-gray-900">
+                        <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
                             Learn more <span aria-hidden="true">→</span>
                         </a>
                     </div>
