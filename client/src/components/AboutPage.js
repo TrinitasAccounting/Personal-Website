4c150f3be10f0f4d61962ab5980ca98357d8aa51
Clay Mangum <clay@trinitasaccounting.com>
2024-05-24 10:52:06 -0400

    Third try to solve the aboutpage deployment

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -15,7 +15,7 @@
   }
   ```
 */
-import { useState } from 'react'
+// import { useState } from 'react'
 // import { Dialog, DialogPanel } from '@headlessui/react'
 // import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
