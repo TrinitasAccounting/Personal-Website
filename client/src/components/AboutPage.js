545239cd0eb27ef8495fc47999e2bf8c29e2ce9d
Clay Mangum <clay@trinitasaccounting.com>
2024-05-25 16:32:30 -0400

    Updated headers on About page

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -58,16 +58,18 @@ const timeline = [
 const jobOpenings = [
     {
         id: 1,
-        role: 'Full-time designer',
+        role: 'Algorithm Developer',
+        company: 'FTMO',
         href: '/',
         description:
-            'Quos sunt ad dolore ullam qui. Enim et quisquam dicta molestias. Corrupti quo voluptatum eligendi autem labore.',
+            'Design and programmed forex trading algorithms for a hedge fund',
         salary: '$75,000 USD',
         location: 'San Francisco, CA',
     },
     {
         id: 2,
         role: 'Laravel developer',
+        company: 'FTMO',
         href: '/',
         description:
             'Et veniam et officia dolorum rerum. Et voluptas consequatur magni sapiente amet voluptates dolorum. Ut porro aut eveniet.',
@@ -77,6 +79,7 @@ const jobOpenings = [
     {
         id: 3,
         role: 'React Native developer',
+        company: 'FTMO',
         href: '/',
         description:
             'Veniam ipsam nisi quas architecto eos non voluptatem in nemo. Est occaecati nihil omnis delectus illum est.',
@@ -277,8 +280,110 @@ export default function AboutPage() {
             </header> */}
 
             <main className="isolate">
+
+                {/* Education Section */}
+                <div className="mx-auto mt-12 max-w-7xl px-2 sm:mt-24 lg:px-2">
+                    <div className="mx-auto max-w-2xl lg:mx-0">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            Education
+                        </h2>
+                        <p className="mt-6 text-base leading-7 text-gray-600">
+                            Diam nunc lacus lacus aliquam turpis enim. Eget hac velit est euismod lacus. Est non placerat nam arcu.
+                            Cras purus nibh cursus sit eu in id. Integer vel nibh.
+                        </p>
+                    </div>
+                    <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+                            <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">Mechanical Engineer</p>
+                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                                <p className="text-lg font-semibold tracking-tight text-gray-900">Users on the platform</p>
+                                <p className="mt-2 text-base leading-7 text-gray-600">
+                                    Vel labore deleniti veniam consequuntur sunt nobis.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
+                            <p className="flex-none text-3xl font-bold tracking-tight text-white">Masters in Business Administration</p>
+                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                                <p className="text-lg font-semibold tracking-tight text-white">
+                                    We’re proud that our customers have made over $8 billion in total revenue.
+                                </p>
+                                <p className="mt-2 text-base leading-7 text-gray-400">
+                                    Eu duis porta aliquam ornare. Elementum eget magna egestas.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-indigo-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+                            <p className="flex-none text-3xl font-bold tracking-tight text-white">Software Engineering Bootcamp</p>
+                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                                <p className="text-lg font-semibold tracking-tight text-white">Transactions this year</p>
+                                <p className="mt-2 text-base leading-7 text-indigo-200">
+                                    Eu duis porta aliquam ornare. Elementum eget magna egestas. Eu duis porta aliquam ornare.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* Work Experience Section */}
+                <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+                    <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
+                        <div className="w-full lg:max-w-lg lg:flex-auto">
+                            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                                Work Experience
+                            </h2>
+                            <p className="mt-6 text-xl leading-8 text-gray-600">
+                                Diam nunc lacus lacus aliquam turpis enim. Eget hac velit est euismod lacus. Est non placerat nam arcu.
+                                Cras purus nibh cursus sit eu in id.
+                            </p>
+                            <img
+                                src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&h=1104&q=80"
+                                alt=""
+                                className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]"
+                            />
+                        </div>
+                        <div className="w-full lg:max-w-xl lg:flex-auto">
+                            <h3 className="sr-only">Job openings</h3>
+                            <ul className="-my-8 divide-y divide-gray-100">
+                                {jobOpenings.map((opening) => (
+                                    <li key={opening.id} className="py-8">
+                                        <dl className="relative flex flex-wrap gap-x-3">
+                                            <dt className="sr-only">Role</dt>
+                                            <dd className="w-full flex-none text-lg font-semibold tracking-tight text-gray-900">
+                                                <a href={opening.href}>
+                                                    {opening.role}
+                                                    <span className="absolute inset-0" aria-hidden="true" />
+                                                </a>
+                                            </dd>
+                                            <dt className="sr-only">Description</dt>
+                                            <dd className="mt-2 w-full flex-none text-base leading-7 text-gray-600">{opening.description}</dd>
+                                            <dt className="sr-only">Salary</dt>
+                                            <dd className="mt-4 text-base font-semibold leading-7 text-gray-900">{opening.salary}</dd>
+                                            <dt className="sr-only">Location</dt>
+                                            <dd className="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
+                                                <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 flex-none fill-gray-300" aria-hidden="true">
+                                                    <circle cx={1} cy={1} r={1} />
+                                                </svg>
+                                                {opening.location}
+                                            </dd>
+                                        </dl>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="mt-8 flex border-t border-gray-100 pt-8">
+                                <a href="/" className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                                    View all openings <span aria-hidden="true">&rarr;</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
                 {/* Hero section */}
-                <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+                <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-7">
                     <div
                         className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
                         aria-hidden="true"
@@ -286,20 +391,20 @@ export default function AboutPage() {
                     <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
                         <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
                             <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-                                We’re a passionate group of people working from around the world to build the future of ecommerce.
+                                Skills
                             </h1>
-                            <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+                            <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-2 xl:row-start-2">
                                 <p className="text-lg leading-8 text-gray-600">
                                     Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
                                     amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui
                                     irure qui lorem cupidatat commodo.
                                 </p>
                             </div>
-                            <img
+                            {/* <img
                                 src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
                                 alt=""
                                 className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
-                            />
+                            /> */}
                         </div>
                     </div>
                     <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
@@ -441,7 +546,7 @@ export default function AboutPage() {
                 </div> */}
 
                 {/* Stats */}
-                <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+                {/* <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                             We approach the workplace as something that adds to our lives and adds value to world.
@@ -482,10 +587,10 @@ export default function AboutPage() {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
 
                 {/* Content section */}
-                <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+                {/* <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
                     <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
                         <div className="w-full lg:max-w-lg lg:flex-auto">
                             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -536,7 +641,7 @@ export default function AboutPage() {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </main>
 
             {/* Footer */}
