8cfee01c8e07c7f6db03530e9240b8c49fad31fb
Clay Mangum <clay@trinitasaccounting.com>
2024-06-15 16:14:56 -0400

    updating project list and images

--- client/src/components/ProjectsList.js
+++ client/src/components/ProjectsList.js
@@ -1,5 +1,7 @@
 
 import FinancialExpenseTrackerDashboardScreenshot from '../images/FinancialExpenseTrackerDashboardScreenshot.png'
+import NBAStatsTracker from '../images/NBAStatsTracker.png';
+import ConstuctASaveDistributorPageScreenshot from '../images/ConstuctASaveDistributorPageScreenshot.png';
 
 const posts = [
     {
@@ -30,8 +32,7 @@ const posts = [
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
         imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
+            ConstuctASaveDistributorPageScreenshot,
         datetime: '2020-03-16',
         buildType: { title: 'Full Stack', stack: 'React, Flask, Postgresql, Tailwind', href: '/' },
         author: {
@@ -51,7 +52,7 @@ const posts = [
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
         imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+            NBAStatsTracker,
         date: 'Mar 16, 2020',
         datetime: '2020-03-16',
         buildType: { title: 'Full Stack', stack: 'React, Flask, Postgresql, Tailwind', href: '/' },
@@ -146,11 +147,11 @@ export default function ProjectsList() {
                     <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
                         {posts.map((post) => (
                             <article key={post.id} className="relative isolate flex flex-col gap-8 lg:flex-row">
-                                <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+                                <div className="relative aspect-[16/9] border-solid border-2 border-indigo-300 sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
                                     <img
                                         src={post.imageUrl}
                                         alt=""
-                                        className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                                        className="absolute inset-0 h-full w-full  bg-gray-50 object-cover"
                                     />
                                     <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                                 </div>
