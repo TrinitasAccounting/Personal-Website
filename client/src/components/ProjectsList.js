0929968e89aa94a0b834c8a6f15703205a9a877e
Clay Mangum <clay@trinitasaccounting.com>
2024-06-16 12:18:14 -0400

    updating the projects list

--- client/src/components/ProjectsList.js
+++ client/src/components/ProjectsList.js
@@ -2,6 +2,7 @@
 import FinancialExpenseTrackerDashboardScreenshot from '../images/FinancialExpenseTrackerDashboardScreenshot.png'
 import NBAStatsTracker from '../images/NBAStatsTracker.png';
 import ConstuctASaveDistributorPageScreenshot from '../images/ConstuctASaveDistributorPageScreenshot.png';
+import TriviaGameScreenshot from '../images/TriviaGameScreenshot.png';
 
 const posts = [
     {
@@ -73,7 +74,7 @@ const posts = [
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
         imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+            TriviaGameScreenshot,
         date: 'Mar 16, 2020',
         datetime: '2020-03-16',
         buildType: { title: 'CLI', stack: 'Python, CLI', href: '/' },
@@ -138,16 +139,16 @@ const posts = [
 export default function ProjectsList() {
     return (
         <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto  max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:max-w-4xl">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Software Projects</h2>
                     {/* <p className="mt-2 text-lg leading-8 text-gray-600">
                         Learn how to grow your business with our expert advice.
                     </p> */}
-                    <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+                    <div className="mt-16 m-4 grid grid-cols-1  space-y-20 lg:mt-20 lg:space-y-20">
                         {posts.map((post) => (
-                            <article key={post.id} className="relative isolate flex flex-col gap-8 lg:flex-row">
-                                <div className="relative aspect-[16/9] border-solid border-2 border-indigo-300 sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+                            <article key={post.id} className="relative isolate  flex flex-col gap-8 lg:flex-row">
+                                <div className="relative aspect-[16/9]  border-solid border-2 border-indigo-300 sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
                                     <img
                                         src={post.imageUrl}
                                         alt=""
