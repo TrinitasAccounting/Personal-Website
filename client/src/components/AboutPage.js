efc8a2d69663c909985ba300ff277a52250152bc
Clay Mangum <clay@trinitasaccounting.com>
2024-07-10 13:53:29 -0400

    Updating my about me and my work experience

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -58,21 +58,20 @@ const timeline = [
 ]
 const jobOpenings = [
     {
-        id: 1,
-        role: 'Algorithm Developer ',
-        company: 'FTMO Hege Fund',
-        timeline: 'Mar 2022 - Present',
-        description:
-            'I developed trading indicators and algorithms using PineScript or MQL5 programming languages for professional traders. The algorithms trade fully automated on the ForEx market, trading $155k in total of the hedge fund\'s capital. I built out the program logic for all algorithms and worked with a developmental team half of the time. Together we built out fully functional algorithms that successfully generated profit for the hedge fund and users.',
-
-    },
-    {
         id: 3,
-        role: 'Junior Software Developer Internship',
+        role: 'Full Stack Software Engineer - Freelance Role',
         company: 'MyPrice.Best',
         timeline: 'May 2024 - Present',
         description:
-            'Working with a start up company in the healthcare industry, to develop and build their software web application. Developing using a stack of Flask backend, Postgresql, Bcrpyt for password authorization, React.js, Tailwind, and Material UI.  ',
+            'Working with a start up company in the healthcare industry, to develop and build their software web application. Building out a full stack application with React.js, Flask, Postgresql database, and hosted on a PaaS. Using React.js and Flask to handle user authentication and authorization. Using React front-end to control what pages and actions are visible to a user based on the type of user logged in. Healthcare company, so client information is extremely confidential. I use Bcrypt library as a encryption to protect all sensitive information ',
+    },
+    {
+        id: 1,
+        role: 'Algorithm Developer / Trader',
+        company: 'FTMO Hege Fund',
+        timeline: 'Mar 2022 - Present',
+        description:
+            'I developed trading indicators and algorithms using PineScript or MQL5 programming languages for professional traders. The algorithms trade fully automated on the ForEx market, trading $165k in total of the hedge fund\'s capital. I built out the program logic for all algorithms and worked with a developmental team half of the time. Together we built out fully functional algorithms that successfully generated profit for the hedge fund and users.',
 
     },
     // {
@@ -97,53 +96,18 @@ const jobOpenings = [
     },
     {
         id: 2,
-        role: 'Founder & Power BI Developer / Analyst',
-        company: 'Trinitas Accounting',
+        role: 'Full Stack - Power BI & Javascript Developer',
+        company: 'Founder/Owner of Trinitas Accounting, LLC',
         timeline: 'Feb 2019 - Feb 2022',
 
         description:
-            'Designed financial software analytics using Power BI and Tableau, that I launched to small business owners. Queried data directly from their accounting software, to present real time financial information to the users (via mobile and desktop). Worked directly with a Quickbooks API and pulled data into a relational database, so Power BI could properly map it. Building our analytic visualizations for the database data overall.Developed and successfully updated & maintained this software for business owners until 2022. Then shifted fully into an outsourced CFO role helping businesses gain funding and/or improve their business’s financial situation.',
-
+            'Designed financial software analytics using Power BI and Tableau, that I launched to small business owners. Queried data directly from their accounting software, to present real time financial information to the users (via mobile and desktop). Worked directly with a Quickbooks API and pulled data into a relational database, so Power BI could properly map it. Building our analytic visualizations for the database data overall. Developed and successfully updated & maintained this software for business owners until 2022.',
     },
 ]
 const footerNavigation = {
 
     social: [
-        // {
-        //     name: 'Blog',
-        //     href: '/',
-        //     icon: (props) => (
-        //         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        //             <path
-        //                 fillRule="evenodd"
-        //                 d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-        //                 clipRule="evenodd"
-        //             />
-        //         </svg>
-        //     ),
-        // },
-        // {
-        //     name: 'Instagram',
-        //     href: '/',
-        //     icon: (props) => (
-        //         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        //             <path
-        //                 fillRule="evenodd"
-        //                 d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-        //                 clipRule="evenodd"
-        //             />
-        //         </svg>
-        //     ),
-        // },
-        // {
-        //     name: 'X',
-        //     href: '/',
-        //     icon: (props) => (
-        //         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        //             <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-        //         </svg>
-        //     ),
-        // },
+
         {
             name: 'GitHub',
             href: 'https://github.com/TrinitasAccounting',
@@ -157,19 +121,7 @@ const footerNavigation = {
                 </svg>
             ),
         },
-        // {
-        //     name: 'YouTube',
-        //     href: '/',
-        //     icon: (props) => (
-        //         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        //             <path
-        //                 fillRule="evenodd"
-        //                 d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-        //                 clipRule="evenodd"
-        //             />
-        //         </svg>
-        //     ),
-        // },
+
     ],
 }
 
@@ -214,7 +166,7 @@ export default function AboutPage() {
                                     Experienced in Python with Flask and JavaScript based programming with a background in finance and analytics. Possess a strong ability to self-learn, work with a team, and solve real world programming problems. Also has an extensive background in (small-medium) technology and emerging software companies. I began self learning 6 years ago and attended Flatiron bootcamp to expand on my knowledge and love of developing software programs to solve everyday problems. My skills/experience can benefit any company looking for someone who can see how a software offering generates revenue for a company and understands how to drive positive impact to the product and company overall.
                                 </p> */}
                                 <p className="text-lg leading-8 text-gray-600">
-                                    I was raised on a small farm in Williamston, South Carolina. I played indoor Mens Volleyball at Clemson University, and pursued my dream playing professional beach volleyball for two years after my undergrad. I am experienced in Python with Flask and JavaScript based programming with a background in finance and analytics. Possess a strong ability to self-learn, work with a team, and solve real world programming problems. Also I have an extensive background in (small-medium) technology and emerging software companies. I began self learning 6 years ago and attended Flatiron bootcamp to expand on my knowledge and love of developing software programs to solve everyday problems. My skills/experience can benefit any company looking for someone who can see how a software offering generates revenue for a company and understands how to drive positive impact to the product and company overall.
+                                    I was raised on a small farm in Williamston, South Carolina. I played indoor Men’s Volleyball at Clemson University, and pursued my dream playing professional beach volleyball for two years after my undergrad. I am a full stack software engineer experienced in React.js, Flask, SQL, etc. Having 3 years of experience in fintech as a full stack engineer and experience starting in March 2024 working as a full stack engineer in the healthcare industry. Quick learner, with the majority of my experience building full stack software using a React frontend, Flask backend, Postgresql database, and hosted on a PaaS. I also have a deep background in finance and analytics, with experience in multiple Saas businesses. I pride myself on my strong ability to self-learn, work with a team, and solve real world programming problems. Also, I have an extensive background in (small-medium) technology and emerging software companies. I attended Flatiron bootcamp to gain structure and expand on my knowledge and love of developing software programs to solve everyday problems. My skills/experience can benefit any company looking for someone who can see how a software offering generates revenue for a company and understands how to drive positive impact to the product and company overall.
                                 </p>
                             </div>
                             {/* <img
@@ -234,7 +186,7 @@ export default function AboutPage() {
                             Skills
                         </h2>
                         <p className="mt-6 text-base leading-7 text-gray-600">
-                            List of skills that I have learned over my 6 years of programming. Starting with collegiate courses, through my bootcamp, and then to me building two software businesses as a founder.
+                            List of skills that I have learned over my 6+ years of programming. Starting with collegiate courses, through my bootcamp, and then to me building two software businesses as a founder.
                         </p>
                     </div>
                     <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
@@ -243,60 +195,6 @@ export default function AboutPage() {
                     </div>
                 </div>
 
-
-
-                {/* Education Section */}
-                {/* <div className="mx-auto mt-12 max-w-7xl px-2 sm:mt-24 lg:px-2">
-                    <div className="mx-auto max-w-2xl lg:mx-0">
-                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                            Education
-                        </h2>
-                        <p className="mt-6 text-base leading-7 text-gray-600">
-                            Diam nunc lacus lacus aliquam turpis enim. Eget hac velit est euismod lacus. Est non placerat nam arcu.
-                            Cras purus nibh cursus sit eu in id. Integer vel nibh.
-                        </p>
-                    </div>
-                    <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
-                        
-                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-200 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
-                            <p className="flex-none text-3xl font-bold tracking-tight text-black">B.S. in Mechanical Engineering</p>
-                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                                <p className="text-lg font-semibold tracking-tight text-black">
-                                    Completed 2 years of C++ and Matlab during my undergradute major
-                                </p>
-                                <p className="mt-2 text-base leading-7 text-gray-700">
-                                    Clemson University
-                                </p>
-                                <p className="text-base leading-7 text-gray-700">
-                                    Graduated in 2017
-                                </p>
-                            </div>
-                        </div>
-                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
-                            <p className="flex-none text-3xl font-bold tracking-tight text-white">Masters in Business Administration</p>
-                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                                <p className="text-lg font-semibold tracking-tight text-white">
-                                    We’re proud that our customers have made over $8 billion in total revenue.
-                                </p>
-                                <p className="mt-2 text-base leading-7 text-gray-400">
-                                    Eu duis porta aliquam ornare. Elementum eget magna egestas.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-indigo-600 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
-                            <p className="flex-none text-3xl font-bold tracking-tight text-white">Software Engineering Bootcamp</p>
-                            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                                <p className="text-lg font-semibold tracking-tight text-white">
-                                    We’re proud that our customers have made over $8 billion in total revenue.
-                                </p>
-                                <p className="mt-2 text-base leading-7 text-gray-400">
-                                    Eu duis porta aliquam ornare. Elementum eget magna egestas.
-                                </p>
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div> */}
 
 
                 {/* Work Experience Section */}
@@ -312,7 +210,7 @@ export default function AboutPage() {
                             <img
                                 src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&h=1104&q=80"
                                 alt=""
-                                className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[1100px]"
+                                className="mt-8 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[1130px]"
                             />
                         </div>
                         <div className="w-full lg:max-w-xl lg:flex-auto">
@@ -372,7 +270,7 @@ export default function AboutPage() {
                             </h1>
                             <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-2 xl:row-start-2">
                                 <p className="text-lg leading-8 text-gray-600">
-                                    How I moved from learning programming as an undergradute, to building a software business during graduate school, and then refinning and deeping my skills specifically related to software engineering.
+                                    How I moved from learning programming during my undergradute degree, to building a software business during graduate school, and then refinning and deeping my skills specifically related to software engineering.
                                 </p>
                             </div>
                             {/* <img
